c13861946a5b874b8e320939e1abb67b2ec570ad
bef4 <befacool@gmail.com>
2023-02-08 21:09:20 +0100

    almost done

--- app.js
+++ app.js
@@ -13,8 +13,52 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
+// image Beer:
+app.get("/",function(req,res){
+  const beerImage = "/images/beer.png"
+
+  res.render("index", {pizza: beerImage})
+})
 
 // ...
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+  .then(beersFromApi =>  {
+    console.log(beersFromApi)
+    res.render('beers', {allBeers: beersFromApi} )}) //sending the array allBeers
+  .catch(error => console.log(error));
+})
+
+app.get("/random-beers",function(req,res){
+  punkAPI.getRandom()
+    .then(randomBeers => {
+      console.log('Beers from the database: ', randomBeers)
+    res.render("random-beers",{newRandomBeers: randomBeers[0]})
+  }) 
+  .catch(error => console.log(error))
+})
+
+app.get("/beer/:id", (req,res) => {
+
+  const id = req.params // with the req.params you can use the information of your ID in the url bar
+  console.log(id)
+  
+  punkAPI.getBeer(req.params.id)
+  .then(getFreshBeer => {
+    console.log(getFreshBeer)
+    res.render("singleBeer", {getFreshBeer : getFreshBeer[0]})
+  } )
+
+
+})
+
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
 
 // Add the route handlers here:
 
